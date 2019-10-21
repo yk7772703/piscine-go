2c5829b081f5e718a943974078fa2bf89015d965
yk7772703 <you@example.comyk7772703@gmail.com>
2019-10-22 00:29:56 +0600

    ggg

---
+++ iterativefactorial.go
@@ -0,0 +1,20 @@
+package piscine
+
+func IterativeFactorial(nb int) int {
+
+	if nb < 0 || nb > 12 {
+		return 0
+	} else if nb == 0 {
+		return 1
+	}
+
+	b := 1
+	c := nb
+	for nb := 1; nb <= c; nb++ {
+
+		b = b * nb
+	}
+
+	return b
+
+}
