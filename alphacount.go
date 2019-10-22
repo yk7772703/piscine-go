96f24cfa6e1ddfed7cedcba89b75f00921378084
yk7772703 <you@example.comyk7772703@gmail.com>
2019-10-22 19:43:19 +0600

    ddd

---
+++ alphacount.go
@@ -0,0 +1,13 @@
+package piscine
+import "fmt"
+func AlphaCount(str string) int {
+    count := 0
+    for i := 0; i < len(str); i++ {
+        count++
+        if !(str[i] >= 'A') && (str[i] <= 'Z') && !(str[i] >= 'a') && (str[i] <= 'z') {
+            count--
+        }
+    }
+    return count
+}
+
