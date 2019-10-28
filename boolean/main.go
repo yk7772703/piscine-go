d4ef0bb13be853637c8505a8ab0df6ea89fa1aba
yk7772703 <you@example.comyk7772703@gmail.com>
2019-10-28 23:03:37 +0600

    ggg

---
+++ boolean/main.go
@@ -0,0 +1,24 @@
+func printStr(str string) {
+	arrayStr := []rune(str)
+
+	for i := 0; i < len(arrayStr); i++ {
+		z01.PrintRune(arrayStr[i])
+	}
+	z01.PrintRune('\n')
+}
+
+func isEven(nbr int) boolean {
+	if even(nbr) == 1 {
+		return yes
+	} else {
+		return no
+	}
+}
+
+func main() {
+	if isEven(lengthOfArg) == 1 {
+		printStr(EvenMsg)
+	} else {
+		printStr(OddMsg)
+	}
+}
